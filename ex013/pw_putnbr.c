9b1f655a5fd1fc7b75073849569b56e6d2d0d19a
randypow42 <randy42power@gmail.com>
2025-05-27 15:25:46 +0200

    ex013: implemnetd putnbr stored in an array

---
+++ ex013/pw_putnbr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+void pw_putnbr(unsigned int nb)
+{
+    int str[20];
+    int i = 0;
+    if (nb == 0)
+    {
+        write(1, '0', 1);
+    
+    }
+    while (nb >= 0)
+    {
+        str[i] = (nb%10) + '0';
+        nb = nb / 10;
+        i++;
+    }
+    while (i--)
+    {
+        write(1, &str[i], 1);
+    }
+}
