3b01741d0af19f3dcb0c9e4998671bc748b9d0e4
randypow42 <randy42power@gmail.com>
2025-05-27 14:40:13 +0200

    ex012: attemppted to calculate binary using an array to disply for 255 lines redo this until u see

---
+++ ex012/pw_print_bits.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+
+void pw_print_bits(void)
+{
+    int num;
+    for (num = 0; num <= 255; num++)
+    {
+        char binary[8];
+        int i;
+
+        
+        for (i = 0; i < 8; i++)
+            binary[i] = '0';
+
+        
+        i = 7;
+        int n = num;
+        while (n > 0 && i >= 0)
+        {
+            binary[i] = (n % 2) + '0';
+            n = n / 2;
+            i--;
+        }
+
+        write(1, binary, 8);
+        write(1, "\n", 1);
+    }
+}
