7a1945e7c48815081a39f68545cfd535b98d597b
randypow42 <randy42power@gmail.com>
2025-05-27 10:00:37 +0200

    ex002: implemented pw_triangle to draw a triangle of a's

---
+++ ex002/pw_triangle.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void pw_triangle(void)
+{
+    int i = 1;
+    while (i <=3)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            write(1, "a", 1);
+            j++;
+        }
+        i++;
+        write(1, "\n", 1);
+    }
+}
